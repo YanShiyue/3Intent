a1aad43bdef2e0cbfcd2ac1095b78fc0eef219cf
YanShiyue <3201892594@qq.com>
2019-12-08 10:25:15 +0800

    finish

--- app/src/main/java/com/example/intent/FirstActivity.java
+++ app/src/main/java/com/example/intent/FirstActivity.java
@@ -2,7 +2,10 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        Button bt_first=(Button)findViewById(R.id.bt_first);
+        bt_first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//隐式启动SecondActivity
+                Intent intent=new Intent("com.example.intent.ACTION_START");
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/intent/SecondActivity.java
+++ app/src/main/java/com/example/intent/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        /*Button bt_second=(Button)findViewById(R.id.bt_second);
+        bt_second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent("android.intent.action.MAIN");
+                intent.addCategory("android.intent.category.LAUNCHER");
+                startActivity(intent);
+            }
+        });*/
     }
 }
